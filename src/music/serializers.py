2bef42ff2ffcb03c0e923958d2747f29f01a9a15
yellnest <20vanyusha03@gmail.com>
2023-05-04 16:38:06 +0300

    Added avg difficult and permission

--- src/music/serializers.py
+++ src/music/serializers.py
@@ -3,14 +3,25 @@ from .models import *
 
 
 class ArtistSerializer(serializers.ModelSerializer):
+    number_of_songs = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = ('id', 'nick', 'slug', 'number_of_songs',)
+
+        nick = models.CharField(max_length=150)
+        slug = models.SlugField(max_length=160, unique=True)
+        image = models.ImageField(upload_to=path_to_artists_upload_image, blank=True, null=True)
+        # fields = '__all__'
 
 
 class MusicSerializer(serializers.ModelSerializer):
+    count_avg = serializers.FloatField(read_only=True)
+
     class Meta:
         model = Music
         fields = (
-            'id', 'song_title', 'slug', 'comment', 'ambiguity', 'slang', 'speech_speed', 'without_text', 'published',
+            'id', 'song_title', 'slug', 'comment', 'ambiguity', 'slang', 'speech_speed', 'without_text', 'count_avg',
+            'published',
+
         )
--- src/music/views.py
+++ src/music/views.py
@@ -1,17 +1,38 @@
-from django.shortcuts import render
+from django.db.models import Count, F
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from .models import *
 from .serializers import ArtistSerializer, MusicSerializer
+from .permission import IsAdminOrReadOnly
 
 
 class ArtistViewSet(viewsets.ModelViewSet):
-    queryset = Artist.objects.all()
+    queryset = Artist.objects.all().annotate(
+        number_of_songs=Count('music')
+    ).order_by('-pk')
+
     serializer_class = ArtistSerializer
     lookup_field = 'slug'
 
+    filter_backends = (SearchFilter,)
+    filter_fields = ('nick', 'number_of_songs')
+    search_fields = ('nick', 'number_of_songs')
+
+    permission_classes = (IsAdminOrReadOnly,)
+
 
 class MusicViewSet(viewsets.ModelViewSet):
-    queryset = Music.objects.all()
+    queryset = Music.objects.all().annotate(
+        count_avg=(F('ambiguity') + F('slang') + F('speech_speed') + F('without_text')) / 4,
+    )
+
     serializer_class = MusicSerializer
     lookup_field = 'slug'
+
+    filter_backends = (SearchFilter,)
+    filter_fields = ('count_avg', 'data')
+    search_fields = ('song_title',)
+
+    permission_classes = (IsAdminOrReadOnly,)
+
